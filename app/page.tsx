cc520be4df97a0b136c43043fe6ad5cbad37043d
John <johnzhengyuliu@gmail.com>
2023-10-09 19:46:14 -0500

    update home page, add opos icons

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex h-[50vh] flex-col items-center justify-center space-y-2">
-      <Button href="/1/1" as={Link}>
+    <div className="flex w-full flex-col items-center justify-center space-y-2">
+      <video className="h-[60vh] " src="/opos.mp4" autoPlay muted loop></video>
+
+      <Button href="/1/1" color="primary" as={Link}>
         Start Course
       </Button>
 
--- components/ModuleSelect.tsx
+++ components/ModuleSelect.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import { Card, CardBody } from "@nextui-org/card";
 
 import { Image } from "@nextui-org/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 // Links to each module page, placeholder description
 const modules = [
@@ -13,51 +10,47 @@ const modules = [
     label: "Client Module",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image:
-      "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperPortal/image.png",
+    image: "/icons/DeveloperPortal.png",
   },
   {
     href: "/2",
     label: "Anchor Module",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image:
-      "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/DeveloperToolkit/image.png",
+    image: "/icons/DeveloperToolkit.png",
   },
   {
     href: "/3",
     label: "Frontend Module",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    image:
-      "https://raw.githubusercontent.com/solana-developers/opos-asset/main/assets/SagaPhone/image.png",
+    image: "/icons/Sagaphone.png",
   },
 ];
 
 export default function ModuleSelect() {
-  const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center space-y-2 ">
       {modules.map((module) => (
-        // <Link href={module.href} key={module.href}>
-        <Card
-          className="w-[40vw] border border-transparent hover:border-gray-400 "
-          isPressable
-          isHoverable
-          onPress={() => router.push(module.href)}
-          key={module.href}
-        >
-          <CardBody className="grid grid-cols-6 items-center gap-6 md:grid-cols-12 md:gap-4">
-            <div className="relative col-span-6 md:col-span-4">
-              <Image src={module.image} />
-            </div>
-            <div className="col-span-6 flex flex-col md:col-span-8">
-              <span className="mb-2 text-lg font-semibold">{module.label}</span>
-              <span>{module.description}</span>
-            </div>
-          </CardBody>
-        </Card>
-        // </Link>
+        <Link href={module.href} key={module.href}>
+          <Card
+            className="w-[40vw] border border-transparent hover:border-gray-400 "
+            isPressable
+            isHoverable
+          >
+            <CardBody className="grid grid-cols-6 items-center gap-6 md:grid-cols-12 md:gap-4">
+              <div className="relative col-span-6 md:col-span-4">
+                <Image src={module.image} />
+              </div>
+              <div className="col-span-6 flex flex-col md:col-span-8">
+                <span className="mb-2 text-lg font-semibold">
+                  {module.label}
+                </span>
+                <span>{module.description}</span>
+              </div>
+            </CardBody>
+          </Card>
+        </Link>
       ))}
     </div>
   );
